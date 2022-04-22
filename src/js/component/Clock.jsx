edcdcb9cf2a21a36ebc804984a553e559be68a64
AlexanderDeMatteo <aldml0201@gmail.com>
2022-04-22 23:16:51 +0000

    l

---
+++ src/js/component/Clock.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Clock = () => {
+	return (
+		<div className="contador" id="reloj">
+			<div className="calendario">
+				<i class="fa-solid fa-clock"></i>
+			</div>
+			<div className="four">0</div>
+			<div className="three">0</div>
+			<div className="two">0</div>
+			<div className="one">0</div>
+		</div>
+	);
+};
---
+++ src/js/component/SecondCounter.jsx
@@ -0,0 +1,5 @@
+import Reacto from "react"
+
+export function SecondCounter(props) {
+     console.log(props.prop)
+}
