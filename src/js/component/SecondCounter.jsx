25e7ec58f621914dba28a1194e9ccc9fd13679a5
AlexanderDeMatteo <aldml0201@gmail.com>
2022-07-22 23:08:20 +0000

    simplecounter-listo

--- src/js/component/SecondCounter.jsx
+++
@@ -1,5 +0,0 @@
-import Reacto from "react"
-
-export function SecondCounter(props) {
-     console.log(props.prop)
-}
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,16 +1,37 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock } from "@fortawesome/free-solid-svg-icons";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-import { Clock } from "./Clock.jsx";
+// variables
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
+	const five = Math.floor((props.second / 10000) % 10);
+	const four = Math.floor((props.second / 1000) % 10);
+	const three = Math.floor((props.second / 100) % 10);
+	const two = Math.floor((props.second / 10) % 10);
+	const one = Math.floor((props.second / 1) % 10);
 	return (
-		<div>
-			<Clock />
-		</div>
+		<>
+			<div className="counter">
+				<div className="innercounter">
+					<div className="clock">
+						<FontAwesomeIcon icon={faClock} />
+					</div>
+					<div className="five"> {five} </div>
+					<div className="four"> {four} </div>
+					<div className="three">{three} </div>
+					<div className="two"> {two} </div>
+					<div className="one"> {one}</div>
+				</div>
+			</div>
+		</>
 	);
+};
+
+Home.propTypes = {
+	second: PropTypes.number,
 };
 
 export default Home;
